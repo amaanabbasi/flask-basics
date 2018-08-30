eff7699842aa14ab61c09934e07ea568bd01506b
kamranrkploy <kamran16alam@gmail.com>
2018-08-30 11:40:55 +0530

    added feature

--- abc.py
+++ abc.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask (__name__) 
 @app.route('/url')
 def homepage():
-    return 'welcome to king'
+    return 'welcome to king. We r in feature branch'
 
 if  __name__ == "__main__":
     app.run(debug=True)
