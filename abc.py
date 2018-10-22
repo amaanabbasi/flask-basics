b6f0a589671af8e9736ed166ad41113f235897c4
amaanabbasi <amaanabbasi99@gmail.com>
2018-10-23 00:04:42 +0530

    Added first template along with animations

--- abc.py
+++ abc.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask (__name__) 
-@app.route('/url')
+@app.route('/')
 def homepage():
-    return 'welcome to king'
+    return render_template('index.html')
 
 if  __name__ == "__main__":
     app.run(debug=True)
