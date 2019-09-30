6716aece9bf50fd102d585a2de9b60b897e65528
Mohammad Abbasi <amaanabbasi99@gmail.com>
2019-09-30 23:43:04 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -6,5 +6,9 @@ app = Flask (__name__)
 def homepage():
     return render_template('index.html')
 
+@app.route('/blockchain')
+def blockchain():
+    return "BlockChain Comming Soon!"
+
 if  __name__ == "__main__":
     app.run(debug=True)
